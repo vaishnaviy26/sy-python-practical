85477c93e56e26c2262305fa5a90a9824a411513
vaishnaviy26 <vaishuuyadav26@gmail.com>
2026-07-27 21:17:43 +0530

    Add files via upload

---
+++ trafic signal.py
@@ -0,0 +1,12 @@
+print("***Trafic Signal Rule***")
+
+signal=input("Enter the signal colour:red")
+
+if signal =="red":
+    print("action:stop")
+
+elif signal =="yellow":
+    print("action:wait")
+
+elif signal =="green":
+    print("action:go")
