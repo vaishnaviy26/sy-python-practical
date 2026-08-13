1dd61135c33298eb586c4d664b42f7311bcacffb
vaishnaviy26 <vaishuuyadav26@gmail.com>
2026-08-13 21:21:02 +0530

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,27 +1,20 @@
-# Simple Billing Calculator for Grocery Shop
-
 print("===== Grocery Shop Billing Calculator =====")
 
-# Input
 item1 = float(input("Enter price of Item 1: "))
 item2 = float(input("Enter price of Item 2: "))
 item3 = float(input("Enter price of Item 3: "))
 
-# Arithmetic Operator
 total_bill = item1 + item2 + item3
 
-# Discount calculation
 if total_bill >= 1000:
-    discount = total_bill * 0.10   # 10% discount
+    discount = total_bill * 0.10   
 elif total_bill >= 500 and total_bill < 1000:
-    discount = total_bill * 0.05   # 5% discount
+    discount = total_bill * 0.05
 else:
     discount = 0
 
-# Final payable amount
 final_amount = total_bill - discount
 
-# Relational and Logical Operators Example
 if total_bill >= 500 and total_bill < 1000:
     category = "Eligible for 5% Discount"
 elif total_bill >= 1000:
@@ -29,7 +22,6 @@ elif total_bill >= 1000:
 else:
     category = "No Discount"
 
-# Output
 print("\n===== BILL =====")
 print("Total Bill      : ₹", total_bill)
 print("Discount        : ₹", discount)
