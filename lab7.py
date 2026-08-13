ca33165c1aeae15177eaaefd3bc88bd679ad1ce6
vaishnaviy26 <vaishuuyadav26@gmail.com>
2026-08-13 21:31:10 +0530

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -1,22 +1,18 @@
 # Text Analyzer Tool
 
-# Input paragraph
 text = input("Enter a paragraph: ")
 
-# Initialize counters
 words = 0
 vowels = 0
 spaces = 0
 characters = len(text)
 
-# Count vowels and spaces using string traversal
 for ch in text:
     if ch.lower() in "aeiou":
         vowels += 1
     if ch == " ":
         spaces += 1
-
-# Count words
+        
 if len(text.strip()) == 0:
     words = 0
 else:
@@ -25,13 +21,11 @@ else:
         if ch == " ":
             words += 1
 
-# String indexing
+
 print("\nFirst Character:", text[0] if len(text) > 0 else "No Text")
 
-# String slicing
 print("First 10 Characters:", text[:10])
 
-# Display results
 print("\n----- Text Analysis -----")
 print("Total Characters:", characters)
 print("Total Words:", words)
